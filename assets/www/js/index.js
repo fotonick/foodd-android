99339aabe2af38315d22ae9e47f68ae3e19649f9
Nickolas Fotopoulos <nickolas.fotopoulos@gmail.com>
2012-11-11 16:36:56 -0800

    Added function to query yelp and get back a parsed object

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -114,5 +114,61 @@ var app = {
         }
 
         navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    },
+    getYelpResults: function() {
+        // Taken quite directly from:
+        // https://github.com/Yelp/yelp-api/blob/master/v2/js/search.html
+        var auth = { 
+            consumerKey: "lcpyjU7MLjcTXKz7PqOe1g", 
+            consumerSecret: "iw0a_CmujGE9uGFji4oj91Twi0o",
+            accessToken: "4EdVlVo3v1Kz04sgXCreRFF3QK7K5GZi",
+            accessTokenSecret: "BlaQZIapwTTmOXJmdFR-LjHHUyU",
+            serviceProvider: { 
+              signatureMethod: "HMAC-SHA1"
+            }
+        };
+
+        var terms = 'Restaurants';
+        var near = 'San+Francisco';  // get from GPS
+
+        var accessor = {
+          consumerSecret: auth.consumerSecret,
+          tokenSecret: auth.accessTokenSecret
+        };
+
+        parameters = [];
+        parameters.push(['term', terms]);
+        parameters.push(['location', near]);
+        parameters.push(['callback', 'cb']);
+        parameters.push(['oauth_consumer_key', auth.consumerKey]);
+        parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+        parameters.push(['oauth_token', auth.accessToken]);
+        parameters.push(['oauth_signature_method', auth.serviceProvider.signatureMethod]);
+
+        var message = { 
+          'action': 'http://api.yelp.com/v2/search',
+          'method': 'GET',
+          'parameters': parameters 
+        };
+
+        OAuth.setTimestampAndNonce(message);
+        OAuth.SignatureMethod.sign(message, accessor);
+
+        var parameterMap = OAuth.getParameterMap(message.parameters);
+        parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature)
+        console.log(parameterMap);
+
+        $.ajax({
+            'url': message.action,
+            'data': parameterMap,
+            'cache': true,
+            'dataType': 'jsonp',
+            'jsonpCallback': false,
+            'success': function(data, textStats, XMLHttpRequest) {
+              console.log(data);
+              var output = data;
+              $("body").append(output);
+            }
+        });
     }
 };
